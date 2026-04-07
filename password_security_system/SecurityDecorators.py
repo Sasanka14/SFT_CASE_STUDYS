bc82953b29c4dfed55278e60115b0e57a999a5b2
sasanka <sasankasekharkundu24@gmail.com>
2026-04-07 22:13:58 +0530

    Unified case study repo

--- password_security_system/SecurityDecorators.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def security_warning(action_name):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print(f"[SECURITY] {action_name} started at {datetime.now()}")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
